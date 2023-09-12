f06d19c6fdfd0a13e6114ec06ecce3b9253454a2
zion928 <zion010928@gmail.com>
2023-09-12 19:26:25 +0900

    Update to.py

--- to.py
+++ to.py
@@ -1,4 +1,4 @@
 # to.py입니다.
 
-TOKEN = 'MTAzNjI1NzQ1MDgwODA2MjAxMg.GN_MP8.FjbrVRBlc-EgLjECYXt-Qx852cdEtqrg3Tgnxk'
-RIOTTOKEN = 'RGAPI-786cc584-69c4-4f44-903e-ff8f353e16da'
+TOKEN = 't'
+RIOTTOKEN = 't'
